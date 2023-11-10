9ca1fd02ea06bb99d01d412b381a2f8b960edb21
wtfabduraim <rahmonkulovabduraim16@gmail.com>
2023-11-10 21:25:48 +0500

    lorem

---
+++ main/main.js
@@ -0,0 +1,41 @@
+var Name = String(prompt("Ism kiriting.."));
+console.log(Name);
+
+var Age = Number(prompt("Yoshingizni kiriting.."));
+console.log(Age);
+
+var w = String(prompt("Otangizni ismini kiriting.."));
+console.log(w);
+
+var q =String(prompt("Onangizni ismini kiriting.."));
+console.log(q);
+
+var e =String(prompt("Qayerda tugulgansiz.."));
+console.log(e);
+
+var r =String(prompt("Oylada nechta farsandsizlar.."));
+console.log(r);
+
+var y =String(prompt("IT-ga qayerda oqigansiz.."));
+console.log(y);
+
+var u =String(prompt("Oylik qancha.."));
+console.log(u);
+
+var i =Number(prompt("2+3 qoshib (0) ga bolsak nechi boladi"))
+console.log(i);
+
+var javob = [
+   "Ism: "+" "+ Name + "\n"+
+   "Yosh: "+" "+ Age + "\n"+
+   "Ism: "+" "+ w + "\n"+
+   "Ism: "+" "+ q + "\n"+
+   "Turar joy: "+" "+ e + "\n"+
+   "Nechta: "+" "+ r + "\n"+
+   "Nomi: "+" "+ y + "\n"+
+   "QANCHA: "+" "+ u + "\n"+
+   "Nechi: "+" "+ + i +"\n"
+]
+
+alert(javob)
+
